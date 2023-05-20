3e3fbb0428537f07f9300dc4e9bfa6619805115e
Darrian Bagley <dbagle14@gmail.com>
2023-05-20 13:25:15 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my Blog</h1>
+      <div className="row row-cols-3">
+        <BlogPostCard />
+        <DynamicBlogPostCard title="My First Blog Post" desc="description" />
+      </div>
+      <div>
+        <Counter />
+      </div>
+      <div>
+        <Profile />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function BlogPostCard() {
+  return (
+    <div className="card">
+      <h2>My First Blog Post</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores eum dignissimos amet officiis saepe. Repudiandae, voluptates earum. Officia ratione quisquam
+        repudiandae. Suscipit placeat temporibus similique beatae nesciunt natus quae.
+      </p>
+      <a href="/blog-post">Read More</a>
+    </div>
+  );
+}
+
+function DynamicBlogPostCard(props) {
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+      <a href={props.link}>Read More</a>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <h1>
+      <button className="btn btn-primary" onClick={() => setCount(count - 1)}>
+        -
+      </button>
+      <span>{count}</span>
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </h1>
+  );
+}
+
+function Profile() {
+  const user = {
+    firstName: "Katherine",
+    lastName: "Johnson",
+    profilePic: "https://i.imgur.com/MK3eW3Am.jpg",
+  };
+
+  return (
+    <div>
+      <h2>
+        {user.firstName} {user.lastName}
+      </h2>
+      <img src={user.profilePic} />
+    </div>
+  );
+}
